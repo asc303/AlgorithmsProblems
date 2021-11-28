6779f3207e9645f059204fe37d0c6da18db6a2ac
asc303 <chauhansam94@gmail.com>
2021-11-28 14:58:03 +0530

    [ashutosh] add:Algrorithms Problem

---
+++ Algorithms-Programs/Algorithms-Programs/Permutation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_Programs
+{
+    class Permutation
+    {
+        public static void Permutate(string str, int start, int end)
+        {
+            if (start == end)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    str = (Swap(str, start, i));
+                    Permutate(str, start + 1, end);
+                    str = Swap(str, start, i);
+                }
+            }
+        }
+        public static string Swap(string word, int start, int end)
+        {
+            char temp;
+            char[] charArray = word.ToCharArray();
+            temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
---
+++ Algorithms-Programs/Algorithms-Programs/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Algorithms_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-Permutation Program, 2-Binary Search, 3-Insertion Sort, 4-Bubble Sort, 5-Merge Sort, 6-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter String to Find Permutation ");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        Permutation.Permutate(str, 0, n - 1);
+                        break;
+                    case 2:
+                        string txtpath = File.ReadAllText(@"D:\git project\AlgorithmsProblems\Algorithms-Programs\Algorithms-Programs\Utility\WordFile.txt");
+                        List<string> words = new List<string>(txtpath.Split());
+                        words.Sort();
+                        BinarySearch.BinarySearchWord(words);
+                        break;
+                    case 3:
+                        InsertionSort insertionSort = new InsertionSort();
+                        int[] arr = { 12, 11, 13, 5, 6 };
+                        insertionSort.Sort(arr);
+                        insertionSort.PrintArray(arr);                        
+                        break;
+                    case 4:
+                        int[] arr1 = { 12, 11, 13, 5, 6 };
+                        BubbleSort.BubbleSortInArray(arr1);
+                        BubbleSort.PrintArray(arr1);
+                        break;
+                    case 5:
+                        int[] arr2 = { 38, 27, 43, 3, 9, 82, 10 };
+                        MergeSort.Sort(arr2, 0, arr2.Length - 1);
+                        MergeSort.PrintArray(arr2);
+                        break;
+                    case 6:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
